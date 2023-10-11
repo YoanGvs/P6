2be9446e7c35f0753b751d63f950e61d156198d7
Yoan Gvs <yoangovaerts@gmail.com>
2023-10-12 01:58:37 +0200

    IW-46 IW-47

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,1 +1,25 @@
-console.log('test')
+console.log('test');
+
+
+// Requête FETCH pour récupérer les données
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(data => {
+    const galleryDiv = document.querySelector('.gallery');
+
+    data.forEach(work => {
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
+
+      img.src = work.imageUrl;
+      img.alt = work.title;
+      img.setAttribute('crossorigin', 'anonymous');  // Ajout de l'attribut crossorigin
+
+      figcaption.textContent = work.title;
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      galleryDiv.appendChild(figure);
+    });
+  });
